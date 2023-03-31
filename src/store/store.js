c31a3afa602babc5691cbc1ab2efe97f2143f39f
Maksim Pavlov <maksim.pavlov@Intradiem.com>
2023-03-31 10:06:35 +0200

    Added and configured redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,23 @@
 import {compose, createStore, applyMiddleware} from 'redux';
 import {logger} from "redux-logger";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage"
 
 import {rootReducer} from "./root-reducer";
 
+// Middlewares
 const middlewares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middlewares))
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// Redux persist
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    // User comes from firebase
+    blacklist: ['user']
+}
+
+const appPersistReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(appPersistReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
